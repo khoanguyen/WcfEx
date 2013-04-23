acac2633a0362911707a052ce4d6e72512d9bcc8
Khoa nguyen <minhkhoa4783@gmail.com>
2013-04-23 17:27:57 +0700

    rename

--- WcfExt/Properties/AssemblyInfo.cs
+++ WcfExt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WcfEx")]
+[assembly: AssemblyTitle("WcfExt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WcfEx")]
+[assembly: AssemblyProduct("WcfExt")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
